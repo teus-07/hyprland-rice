a90b14d05b755c222d3e5403ba5588e7bcadf506
teus07 <mateusevangelistaslmg@gmail.com>
2025-08-31 23:22:13 -0300

    Adiconado LazyVim e configurado o NVIM com alguns plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,2 @@
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
--- lazy.nvim bootstrap
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-
-
---TEMA
---vim.cmd.colorscheme("catppuccin")
-
---NEOTREE
---vim.keymap.set('e', '<C-e>', ':Neotree filesystem reveal left')
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
